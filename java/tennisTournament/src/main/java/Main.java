55814ec84858c06c1930acd123fb466dbb4ab0d1
c_akazakov <c_akazakov@groupon.com>
2022-08-09 14:58:12 +0300

    https://app.codility.com/programmers/trainings/3/tennis_tournament/

---
+++ java/tennisTournament/src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(solution(5, 7));
+    }
+
+//    given the number of players P and the number of reserved courts C,
+//    returns the maximum number of games that can be played in parallel
+
+    public static int solution(int P, int C) {
+        int possibleGames = P/2;
+        return possibleGames > C ? C : possibleGames;
+    }
+}
